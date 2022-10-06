3b926ff03fb9558db70ec14178720c645697b34d
Gustavo Lozada <gu.lozada9@gmail.com>
2022-10-05 23:03:41 -0400

    Add persist_data_db to write_data

--- backend/box/data_save.py
+++ backend/box/data_save.py
@@ -2,6 +2,8 @@ import csv
 import time
 from datetime import datetime
 
+from db.functions import persist_data_db
+
 headers = ('temp_1', 'temp_2', 'temp_3', 'temp_4', 'temp_5', 'temp_6',
          'temp_7', 'space', 'temp_8', 'temp_9', 'temp_10', 'temp_11',
          'temp_12',"Heat_flux_1", "Heat_flux_2","Power_input","conductivity", "tiempo")
@@ -36,7 +38,8 @@ def write_data(time_date, data, Heat_flux_1,Heat_flux_2, power,con,file):
     dict_data['Heat_flux_2'] = Heat_flux_2
     dict_data['Power_input'] = power
     dict_data['conductivity'] = con
-    for i, dat in enumerate(data,1):
+    for i, dat in enumerate(data, 1):
         dict_data[f"temp_{i}"] = dat
+
     persist_data(dict_data, file)
-    # persist_data_db(dict_data)
+    persist_data_db(dict_data)
