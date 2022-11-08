28b4d36f458a574a6ad0d2dacb2d4f5954df5a3a
Gustavo Lozada <gu.lozada9@gmail.com>
2022-11-07 23:45:50 -0400

    Updata MongoDBClient to recieve settings in constructor

--- backend/db/client.py
+++ backend/db/client.py
@@ -1,35 +1,32 @@
 from typing import Iterable, Mapping
 
-from decouple import config
 from pymongo import MongoClient
-
-COLLECTION_NAME = "measurements"
 
 
 class MongoDBClient:
 
-    def __init__(self):
-        DB_USERNAME = config('MONGO_USERNAME')
-        DB_PASSWORD = config('MONGO_PASSWORD')
-        self.DB_NAME = config('MONGO_DBNAME')
+    def __init__(self, username: str, password: str, db_name: str,
+                 collection_name: str):
+        self.db_name = db_name
+        self.collection_name = collection_name
 
         self.connection_string = (
             "mongodb://"
-            f"{DB_USERNAME}:{DB_PASSWORD}@localhost:27017"
-            f"/{self.DB_NAME}?authSource=admin&retryWrites=true&w=majority")
+            f"{username}:{password}@localhost:27017"
+            f"/{db_name}?authSource=admin&retryWrites=true&w=majority")
 
     def persist_data_db(self, data: dict):
-        collection = self.connection[COLLECTION_NAME]
+        collection = self.connection[self.collection_name]
         collection.insert_one(data)
 
     def get_data_db(self, quantity: int) -> Iterable[Mapping]:
-        collection = self.connection[COLLECTION_NAME]
+        collection = self.connection[self.collection_name]
         data = collection.find().sort("_id", -1).limit(quantity)
 
         return data
 
     def __enter__(self):
-        self.connection = MongoClient(self.connection_string)[self.DB_NAME]
+        self.connection = MongoClient(self.connection_string)[self.db_name]
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
